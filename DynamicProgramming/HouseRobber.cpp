9c8dfb98452cad94854e54d3fe0969bc1df7613b
Tran Anh Huy <anhhuyrubic@gmail.com>
2025-10-05 23:27:44 +0700

    feat(DynamicProgramming): add House Robber problem implementation

---
+++ DynamicProgramming/HouseRobber.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int rob(vector<int>& nums){
+    int n = nums.size();
+    vector<int> dp(n);
+
+    dp[0] = nums[0];
+    dp[1] = max(nums[0], nums[1]);
+
+    for(int i = 2; i < n; i++){
+        dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+    }
+    return dp[n - 1];
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    #ifndef ONLINE_JUDGE
+        freopen("D:/AnhHuy/Code/C++/input.inp", "r", stdin);
+        freopen("D:/AnhHuy/Code/C++/output.out", "w", stdout);
+    #endif
+      
+    int n; cin >> n;
+    vector<int> nums(n);
+    for(int i = 0; i < n; i++) cin >> nums[i];
+    int ans = rob(nums);
+    cout << ans;
+    return 0;
+}
