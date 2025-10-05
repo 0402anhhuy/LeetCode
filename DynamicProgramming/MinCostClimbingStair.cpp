afbb7c4ccfd71c500e6fb02542f612f92bbe0319
Tran Anh Huy <anhhuyrubic@gmail.com>
2025-10-05 23:27:27 +0700

    feat(DynamicProgramming): add Min Cost Climbing Stairs program skeleton

---
+++ DynamicProgramming/MinCostClimbingStair.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    #ifndef ONLINE_JUDGE
+        freopen("D:/AnhHuy/Code/C++/input.inp", "r", stdin);
+        freopen("D:/AnhHuy/Code/C++/output.out", "w", stdout);
+    #endif
+      
+    
+    return 0;
+}
