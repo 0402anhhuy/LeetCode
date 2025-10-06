96bc97b3ec350ef464c36ac6bbb02d5645337baf
Tran Anh Huy <anhhuyrubic@gmail.com>
2025-10-07 01:03:35 +0700

    refactor(ClimbingStair): remove file redirection for local testing

--- DynamicProgramming/ClimbingStair.cpp
+++ DynamicProgramming/ClimbingStair.cpp
@@ -26,11 +26,6 @@ int climbStairs(int n){
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    #ifndef ONLINE_JUDGE
-        freopen("D:/AnhHuy/Code/C++/input.inp", "r", stdin);
-        freopen("D:/AnhHuy/Code/C++/output.out", "w", stdout);
-    #endif
       
     int n; cin >> n;
     int ans = climbStairs(n);
