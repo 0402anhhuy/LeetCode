050921883bb92d4983364b77c3f25c7015a03b38
Tran Anh Huy <anhhuyrubic@gmail.com>
2025-10-07 01:02:12 +0700

    refactor(HouseRobber): remove file redirection for local testing

--- DynamicProgramming/HouseRobber.cpp
+++ DynamicProgramming/HouseRobber.cpp
@@ -17,11 +17,6 @@ int rob(vector<int>& nums){
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    #ifndef ONLINE_JUDGE
-        freopen("D:/AnhHuy/Code/C++/input.inp", "r", stdin);
-        freopen("D:/AnhHuy/Code/C++/output.out", "w", stdout);
-    #endif
       
     int n; cin >> n;
     vector<int> nums(n);
