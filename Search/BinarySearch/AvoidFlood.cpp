5483a7a06e0b7d4a77fd704758a048cade6ae84f
Tran Anh Huy <anhhuyrubic@gmail.com>
2025-10-08 00:32:51 +0700

    feat(AvoidFlood): implement avoidFlood function to manage lake flooding

---
+++ Search/BinarySearch/AvoidFlood.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr)
+
+using ll  = long long;
+using ull = unsigned long long;
+using ld  = long double;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = vector<int>;
+using vll = vector<ll>;
+using vii = vector<pii>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define eb emplace_back
+#define F first
+#define S second
+
+#define FOR(i, a, b) for (int i = (a); i <= (b); ++i)
+#define FORD(i, a, b) for (int i = (a); i >= (b); --i)
+#define FORI(i, n) for (int i = 0; i < (n); ++i)
+#define REP(i, n) for (int i = 0; i < (n); ++i)
+#define REPD(i, n) for (int i = (n)-1; i >= 0; --i)
+
+const int INF = 1e9 + 7;
+const ll  LINF = 4e18;
+const ld  EPS = 1e-9;
+const int MOD = 1e9 + 7;
+
+vector<int> avoidFlood(vector<int>& rains){
+    int n = rains.size();
+    vector<int> ans(n, 1);
+    unordered_map<int, int> full;
+    set<int> dryDays;
+
+    for(int i = 0; i < n; i++){
+        if(rains[i] > 0){
+            int lake = rains[i];
+            ans[i] = -1;
+            if(full.count(lake)){
+                auto it = dryDays.upper_bound(full[lake]);
+                if(it == dryDays.end()) return {};
+                ans[*it] = lake;
+                dryDays.erase(it);
+            }
+            full[lake] = i;
+        }
+        else{
+            dryDays.insert(i);
+        }
+    }
+    return ans;
+}
+
+int main(){
+    FAST_IO;
+
+    int n; cin >> n;
+    vector<int> rains(n);
+    FORI(i, n) cin >> rains[i];
+
+    vector<int> ans = avoidFlood(rains);
+    if(ans.empty()){
+        cout << "[]";
+    } 
+    else {
+        for(int x : ans) cout << x << " ";
+    }
+    return 0;
+}
