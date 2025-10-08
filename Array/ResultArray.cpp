d5398ba12192503ef5768b95293e5bed751a5d0a
Tran Anh Huy <anhhuyrubic@gmail.com>
2025-10-08 18:47:22 +0700

    feat(Array): add resultArray function to organize elements into two arrays based on comparison

---
+++ Array/ResultArray.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr)
+
+using ll  = long long;
+using ull = unsigned long long;
+using ld  = long double;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = vector<int>;
+using vll = vector<ll>;
+using vii = vector<pii>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define eb emplace_back
+#define F first
+#define S second
+
+#define FOR(i, a, b) for (int i = (a); i <= (b); ++i)
+#define FORD(i, a, b) for (int i = (a); i >= (b); --i)
+#define FORI(i, n) for (int i = 0; i < (n); ++i)
+#define REP(i, n) for (int i = 0; i < (n); ++i)
+#define REPD(i, n) for (int i = (n)-1; i >= 0; --i)
+
+const int INF = 1e9 + 7;
+const ll  LINF = 4e18;
+const ld  EPS = 1e-9;
+const int MOD = 1e9 + 7;
+
+vector<int> resultArray(vector<int>& nums) {
+    vector<int> arr1, arr2;
+    arr1.push_back(nums[0]);
+    arr2.push_back(nums[1]);
+
+    for(int i = 2; i < nums.size(); i++){
+        if(arr1.back() > arr2.back()){
+            arr1.push_back(nums[i]);
+        }
+        else arr2.push_back(nums[i]);
+    }
+
+    arr1.insert(arr1.end(), arr2.begin(), arr2.end());
+    return arr1;
+}
+
+int main(){
+    FAST_IO;
+
+    int n; cin >> n;
+    vector<int> nums(n);
+    FORI(i, n) cin >> nums[i];
+
+    vector<int> ans = resultArray(nums);
+    for(auto i : ans) cout << i << " ";
+    return 0;
+}
