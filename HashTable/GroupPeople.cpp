ebfb4b09b229bb42e2ddf705ea9fd42757c7457f
Tran Anh Huy <anhhuyrubic@gmail.com>
2025-10-09 18:33:54 +0700

    feat(HashTable): implement groupThePeople function to group people based on sizes

---
+++ HashTable/GroupPeople.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr)
+
+using ll  = long long;
+using ull = unsigned long long;
+using ld  = long double;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = vector<int>;
+using vll = vector<ll>;
+using vii = vector<pii>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define eb emplace_back
+#define F first
+#define S second
+
+#define FOR(i, a, b) for (int i = (a); i <= (b); ++i)
+#define FORD(i, a, b) for (int i = (a); i >= (b); --i)
+#define FORI(i, n) for (int i = 0; i < (n); ++i)
+#define REP(i, n) for (int i = 0; i < (n); ++i)
+#define REPD(i, n) for (int i = (n)-1; i >= 0; --i)
+
+const int INF = 1e9 + 7;
+const ll  LINF = 4e18;
+const ld  EPS = 1e-9;
+const int MOD = 1e9 + 7;
+
+vector<vector<int>> groupThePeople(vector<int>& groupSizes){
+    map<int, vector<int>> groups;
+    vector<vector<int>> ans;
+
+    int n = groupSizes.size();
+    for(int i = 0; i < n; i++){
+        groups[groupSizes[i]].push_back(i);
+        if(groups[groupSizes[i]].size() == groupSizes[i]){
+            ans.push_back(groups[groupSizes[i]]);
+            groups[groupSizes[i]].clear();
+        }
+    }
+    return ans;
+}
+
+int main(){
+    FAST_IO;
+
+    int n; cin >> n;
+    vector<int> groupSizes(n);
+    FORI(i, n) cin >> groupSizes[i];
+
+    vector<vector<int>> results = groupThePeople(groupSizes);
+    for(auto result : results){
+        for(int ans : result){
+            cout << ans << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
