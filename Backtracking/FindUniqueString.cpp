b20aec63ac3b94b543dd92163c633d632ca841fe
Tran Anh Huy <anhhuyrubic@gmail.com>
2025-10-16 21:34:02 +0700

    feat(Backtracking): implement findDifferentBinaryString to find a unique binary string

---
+++ Backtracking/FindUniqueString.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr)
+
+using ll  = long long;
+using ull = unsigned long long;
+using ld  = long double;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = vector<int>;
+using vll = vector<ll>;
+using vii = vector<pii>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define eb emplace_back
+#define F first
+#define S second
+
+#define FOR(i, a, b) for (int i = (a); i <= (b); ++i)
+#define FORD(i, a, b) for (int i = (a); i >= (b); --i)
+#define FORI(i, n) for (int i = 0; i < (n); ++i)
+#define REP(i, n) for (int i = 0; i < (n); ++i)
+#define REPD(i, n) for (int i = (n)-1; i >= 0; --i)
+
+const int INF = 1e9 + 7;
+const ll  LINF = 4e18;
+const ld  EPS = 1e-9;
+const int MOD = 1e9 + 7;
+
+
+void backtrack(string s, int n, vector<string> &temp){
+    if(s.length() == n){
+        temp.push_back(s);
+        return;
+    }
+
+    for(int i = '0'; i <= '1'; i++){
+        s += i;
+        backtrack(s, n, temp);
+        s.pop_back();
+    }
+}
+
+string findDifferentBinaryString(vector<string>& nums){
+    int n = nums[0].length();
+
+    vector<string> temp;
+    string s = "";
+    backtrack(s, n, temp);
+
+    set<string> tempStr(nums.begin(), nums.end());
+    for(string ans : temp){
+        if(tempStr.count(ans) == 0){
+            return ans;
+        }
+    }
+    return "a";
+}
+
+int main(){
+    FAST_IO;
+
+    
+    return 0;
+}
