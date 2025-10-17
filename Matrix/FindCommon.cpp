cf8d284047ef1ba5226e2bb48d0d402748c1c21d
Tran Anh Huy <anhhuyrubic@gmail.com>
2025-10-17 23:32:01 +0700

    feat(Matrix): add FindCommon function to determine the most common response from a list of string vectors

---
+++ Matrix/FindCommon.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr)
+
+using ll  = long long;
+using ull = unsigned long long;
+using ld  = long double;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = vector<int>;
+using vll = vector<ll>;
+using vii = vector<pii>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define eb emplace_back
+#define F first
+#define S second
+
+#define FOR(i, a, b) for (int i = (a); i <= (b); ++i)
+#define FORD(i, a, b) for (int i = (a); i >= (b); --i)
+#define FORI(i, n) for (int i = 0; i < (n); ++i)
+#define REP(i, n) for (int i = 0; i < (n); ++i)
+#define REPD(i, n) for (int i = (n)-1; i >= 0; --i)
+
+const int INF = 1e9 + 7;
+const ll  LINF = 4e18;
+const ld  EPS = 1e-9;
+const int MOD = 1e9 + 7;
+
+string findCommonResponse(vector<vector<string>>& responses){
+    map<string, int> strFreq;
+    for(auto &str : responses){
+        set<string> temp(str.begin(), str.end());
+        for(auto it : temp){
+            strFreq[it]++;
+        }
+    }
+
+    int maxCount = 0;
+    string ans = "";
+    for(auto &[s, f] : strFreq){
+        if(f > maxCount || (f == maxCount && s < ans)){
+            maxCount = f;
+            ans = s;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    FAST_IO;
+
+    int n, m; cin >> n;
+    string s;
+    vector<vector<string>> responses(n);
+    FORI(i, n){
+        cin >> m;
+        FORI(j, m){
+            cin >> s;
+            responses[i].push_back(s);
+        }  
+    }
+
+    string ans = findCommonResponse(responses);
+    cout << ans;
+    return 0;
+}
