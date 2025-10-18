dc365e0727ffba8d7632fe07c66e385a417b312f
Tran Anh Huy <anhhuyrubic@gmail.com>
2025-10-18 15:53:49 +0700

    feat(Sort): add MaxDistinct function to calculate maximum distinct elements with a given range

---
+++ Sort/MaxDistinct-LC3397.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr)
+
+using ll  = long long;
+using ull = unsigned long long;
+using ld  = long double;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = vector<int>;
+using vll = vector<ll>;
+using vii = vector<pii>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define eb emplace_back
+#define F first
+#define S second
+
+#define FOR(i, a, b) for (int i = (a); i <= (b); ++i)
+#define FORD(i, a, b) for (int i = (a); i >= (b); --i)
+#define FORI(i, n) for (int i = 0; i < (n); ++i)
+#define REP(i, n) for (int i = 0; i < (n); ++i)
+#define REPD(i, n) for (int i = (n)-1; i >= 0; --i)
+
+const int INF = 1e9 + 7;
+const ll  LINF = 4e18;
+const ld  EPS = 1e-9;
+const int MOD = 1e9 + 7;
+
+int maxDistinctElements(vector<int>& nums, int k){
+    sort(nums.begin(), nums.end());
+
+    int cnt = 0, prev = INT_MIN;
+    for(int num : nums){
+        int curr = min(max(num - k, prev + 1), num + k);
+        if(curr > prev){
+            cnt++;
+            prev = curr;
+        }
+    }
+    return cnt;
+}
+
+int main(){
+    FAST_IO;
+
+    int n, k; cin >> n >> k;
+    vector<int> nums(n);
+    FORI(i, n) cin >> nums[i];
+
+    int ans = maxDistinctElements(nums, k);
+    cout << ans;
+    return 0;
+}
