8cdcd6e19b46cdf14fac630c07fae35573798ca3
Tran Anh Huy <anhhuyrubic@gmail.com>
2025-10-19 18:46:13 +0700

    feat(MathTheory): add ReorderedPowerOf2.cpp to check if a number can be reordered to form a power of two

---
+++ MathTheory/ReorderedPowerOf2.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr)
+
+using ll  = long long;
+using ull = unsigned long long;
+using ld  = long double;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = vector<int>;
+using vll = vector<ll>;
+using vii = vector<pii>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define eb emplace_back
+#define F first
+#define S second
+
+#define FOR(i, a, b) for (int i = (a); i <= (b); ++i)
+#define FORD(i, a, b) for (int i = (a); i >= (b); --i)
+#define FORI(i, n) for (int i = 0; i < (n); ++i)
+#define REP(i, n) for (int i = 0; i < (n); ++i)
+#define REPD(i, n) for (int i = (n)-1; i >= 0; --i)
+
+const int INF = 1e9 + 7;
+const ll  LINF = 4e18;
+const ld  EPS = 1e-9;
+const int MOD = 1e9 + 7;
+
+bool reorderedPowerOf2(int n){
+    string s = to_string(n);
+    sort(s.begin(), s.end());
+
+    for(int i = 0; i < 31; i++){
+        int power = 1 << i;
+        string t = to_string(power);
+        sort(t.begin(), t.end());
+        if(s == t) return true;
+    }
+
+    return false;
+}
+
+int main(){
+    FAST_IO;
+
+    int n; cin >> n;
+    cout << boolalpha << reorderedPowerOf2(n); 
+    return 0;
+}
