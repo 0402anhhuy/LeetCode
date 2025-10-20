de8c6f47f5b27902581a9aeb02120316181245c2
Tran Anh Huy <anhhuyrubic@gmail.com>
2025-10-20 09:32:21 +0700

    feat(String): add ConstructStringRepeatLimit.cpp to implement a function for constructing a string with repeat limits

---
+++ Heap/ConstructStringRepeatLimit.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr)
+
+using ll  = long long;
+using ull = unsigned long long;
+using ld  = long double;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = vector<int>;
+using vll = vector<ll>;
+using vii = vector<pii>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define eb emplace_back
+#define F first
+#define S second
+
+#define FOR(i, a, b) for (int i = (a); i <= (b); ++i)
+#define FORD(i, a, b) for (int i = (a); i >= (b); --i)
+#define FORI(i, n) for (int i = 0; i < (n); ++i)
+#define REP(i, n) for (int i = 0; i < (n); ++i)
+#define REPD(i, n) for (int i = (n)-1; i >= 0; --i)
+
+const int INF = 1e9 + 7;
+const ll  LINF = 4e18;
+const ld  EPS = 1e-9;
+const int MOD = 1e9 + 7;
+
+/*
+    LeetCode 2182. Construct String With Repeat Limit
+
+    Đề bài:
+        Cho chuỗi s và số nguyên repeatLimit
+        Tạo ra một chuỗi mới (repeatLimitedString) từ các ký tự của s sao cho:
+            - Không ký tự nào xuất hiện quá repeatLimit lần liên tiếp
+            - Chuỗi tạo ra phải là **lớn nhất theo thứ tự từ điển (lexicographically largest)** có thể
+            (Không bắt buộc phải dùng hết các ký tự trong s)
+
+    Ví dụ:
+        s = "cczazcc", repeatLimit = 3  →  Output: "zzcccac"
+        s = "aababab", repeatLimit = 2  →  Output: "bbabaa"
+*/
+
+
+int main(){
+    FAST_IO;
+
+    
+    return 0;
+}
