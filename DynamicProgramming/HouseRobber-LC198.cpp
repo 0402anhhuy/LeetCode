206e2aeee1974bbef551789ded3a7fa0d7a641c1
Tran Anh Huy <anhhuyrubic@gmail.com>
2026-01-24 08:22:11 +0700

    feat(DynamicProgramming): enhance rob function to handle single house case and improve code structure

--- DynamicProgramming/HouseRobber-LC198.cpp
+++ DynamicProgramming/HouseRobber-LC198.cpp
@@ -1,6 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr)
+
+using ll  = long long;
+using ull = unsigned long long;
+using ld  = long double;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi  = vector<int>;
+using vll = vector<ll>;
+using vii = vector<pii>;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define eb emplace_back
+#define F first
+#define S second
+
+#define FOR(i, a, b) for (int i = (a); i <= (b); ++i)
+#define FORD(i, a, b) for (int i = (a); i >= (b); --i)
+#define FORI(i, n) for (int i = 0; i < (n); ++i)
+#define REP(i, n) for (int i = 0; i < (n); ++i)
+#define REPD(i, n) for (int i = (n)-1; i >= 0; --i)
+
+const int INF = 1e9 + 7;
+const ll  LINF = 4e18;
+const ld  EPS = 1e-9;
+const int MOD = 1e9 + 7;
+
 /*
     LeetCode 198. House Robber
 
@@ -22,6 +52,8 @@ using namespace std;
 
 int rob(vector<int>& nums){
     int n = nums.size();
+    if(n == 1) return nums[0];
+    
     vector<int> dp(n);
 
     dp[0] = nums[0];
@@ -30,16 +62,17 @@ int rob(vector<int>& nums){
     for(int i = 2; i < n; i++){
         dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
     }
+
     return dp[n - 1];
 }
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-      
+    FAST_IO;
+
     int n; cin >> n;
     vector<int> nums(n);
-    for(int i = 0; i < n; i++) cin >> nums[i];
+   
+    FORI(i, n) cin >> nums[i];
     int ans = rob(nums);
     cout << ans;
     return 0;
